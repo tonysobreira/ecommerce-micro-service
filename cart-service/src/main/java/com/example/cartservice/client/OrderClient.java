404ad8166ac198d6c8227f0d8adc643d867c106d
tonysobreira <tonysobreira@gmail.com>
2026-03-22 18:49:08 -0300

    update

--- cart-service/src/main/java/com/example/cartservice/client/OrderClient.java
+++ cart-service/src/main/java/com/example/cartservice/client/OrderClient.java
@@ -4,10 +4,11 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import com.example.cartservice.config.FeignConfig;
 import com.example.cartservice.dto.request.CreateOrderRequest;
 import com.example.cartservice.dto.response.OrderResponse;
 
-@FeignClient(name = "order-service", path = "/orders")
+@FeignClient(name = "order-service", path = "/orders", configuration = FeignConfig.class)
 public interface OrderClient {
 
 	@PostMapping
--- cart-service/src/main/java/com/example/cartservice/client/ProductClient.java
+++ cart-service/src/main/java/com/example/cartservice/client/ProductClient.java
@@ -4,12 +4,13 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.cartservice.config.FeignConfig;
 import com.example.cartservice.dto.response.ProductQuoteResponse;
 
-@FeignClient(name = "product-service", path = "/internal/products")
+@FeignClient(name = "${product-service.name:product-service}", path = "/internal", configuration = FeignConfig.class)
 public interface ProductClient {
 
-	@GetMapping("/quote")
+	@GetMapping("/products/quote")
 	ProductQuoteResponse quote(@RequestParam("ids") String ids);
 
 }
